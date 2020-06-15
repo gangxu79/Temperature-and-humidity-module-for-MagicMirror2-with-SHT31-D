9709a96630663976a11c89a8e94f178e81e6c9a8
Gang <gangxu79@gmail.com>
2020-06-15 11:48:31 -0400

    Add files via upload

---
+++ ReadHDC1080_Pi.c
@@ -0,0 +1,59 @@
+/*
+ * ReadHDC1080.c
+ *
+ *This program reads the temperature and humidity from a i2c-connected
+ * HDC1080 sensor.
+ *
+ * Returnvalues: 0=Temperature + humidty read successfully
+ * 					1=Temperature error
+ * 					2=Humidity error
+ * 					-1=Temperature + humidity error
+ *
+ *Call example: int iHDC1080Result = ReadHDC1080();
+ *
+ *  Created on: 24.09.2016
+ *      Author: Michael f.
+ */
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c.h>
+#include <string.h>
+#include <linux/i2c-dev.h>
+#include <sys/stat.h>
+
+
+float fTemp=0, fHumid=0;
+
+int main()
+{
+	// Create I2C bus
+	int file;
+	file = open("/dev/i2c-1", O_RDWR);
+	// Get I2C device, SHT31 I2C address is 0x44(68)
+	ioctl(file, I2C_SLAVE, 0x44);
+
+	// Send high repeatability measurement command
+	// Command msb, command lsb(0x2C, 0x06)
+	char config[2] = {0};
+	config[0] = 0x2C;
+	config[1] = 0x06;
+	write(file, config, 2);
+	sleep(1);
+
+	// Read 6 bytes of data
+	// temp msb, temp lsb, temp CRC, humidity msb, humidity lsb, humidity CRC
+	char data[6] = {0};
+	read(file, data, 6);
+	close(file);
+	// Convert the data
+	fTemp = (((data[0] * 256) + data[1]) * 315.0) / 65535.0 - 49.0;
+	fHumid = (((data[3] * 256) + data[4])) * 100.0 / 65535.0;
+	printf("%.3f,%.3f\n", fTemp, fHumid);
+	return(0);
+}
